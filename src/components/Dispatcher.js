fb05c486d17d0c4ddbb3d31d1ad872f98b6be5dc
Francesco <francesco.miccolis98@libero.it>
2021-07-16 22:04:14 +0200

    creato componente homepage

--- src/components/Dispatcher.js
+++ src/components/Dispatcher.js
@@ -1,16 +1,15 @@
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Startups from "../pages/Startups";
 import Projects from "../pages/Projects";
+import Homepage from "../pages/Homepage";
 
 function Dispatcher() {
     return (
         <Router>
             <Switch>
-                <Route exact path="/">
-                    <p>This is the home page</p>
-                </Route>
-                <Route path="/startups" component={Startups} />
-                <Route path="/projects" component={Projects} />
+                <Route exact path="/" component={Homepage} />
+                <Route exact path="/startups" component={Startups} />
+                <Route exact path="/projects" component={Projects} />
             </Switch>
         </Router>
     );
